4ceb4db6c10bdd308ce2fcebb189634c2a657399
Benoit HOFFMAN <benoit.hoffman@epitech.eu>
2019-07-18 15:38:17 +0200

    SETUP - Creation of repository with local project

---
+++ Controlers/CharacterListUserControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DDFight.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DDFight.Controlers
+{
+    /// <summary>
+    /// Interaction logic for CharacterListUserControl.xaml
+    /// </summary>
+    public partial class CharacterListUserControl : UserControl
+    {
+        public CharacterListUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void AddCharacter(object sender, RoutedEventArgs e)
+        {
+            NewCharacterWindow character = new NewCharacterWindow();
+            character.ShowDialog();
+        }
+
+        private void RemoveCharacter(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Windows/NewCharacterDataContext.cs
@@ -0,0 +1,33 @@
+﻿// /***********************************************************************************
+//  * ERTMS Solutions
+//  ***********************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDFight.Windows
+{
+    public class NewCharacterDataContext
+    {
+        public string Name { get; set; }
+
+        public string raw_CA { get; set; }
+
+        public int CA;
+
+        public string raw_initiative { get; set; }
+
+        public int Initiative;
+
+        public string raw_max_hp { get; set; }
+
+        public int RawMaxHP;
+
+        public string raw_hp { get; set; }
+
+        public int MaxHp;
+    }
+}
