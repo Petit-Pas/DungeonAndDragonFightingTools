df478899a4fa232d302ed481a449c0b207acb06f
Benoit <benoit.hoffman@epitech.eu>
2020-01-07 21:17:10 +0100

    DEV - Modified EditableDiceRollUSerControl with a PropertyPath

---
+++ DDFight/Controlers/Game/Dices/EditableDiceRollUserControl.xaml.cs
@@ -0,0 +1,86 @@
+﻿using DDFight.Converters;
+using DDFight.Game.Aggression.Attacks;
+using DDFight.Game.Dices;
+using DDFight.ValidationRules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DDFight.Controlers.Game.Dices
+{
+    /// <summary>
+    /// Interaction logic for DiceRollUserControl.xaml
+    /// </summary>
+    public partial class EditableDiceRollUserControl : UserControl, IValidable
+    {
+        public EditableDiceRollUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public String PropertyPath
+        {
+            get { return (String)GetValue(PropertyPathProperty); }
+            set { SetValue(PropertyPathProperty, value); }
+        }
+
+        public static readonly DependencyProperty PropertyPathProperty =
+            DependencyProperty.Register(nameof(PropertyPath), typeof(String),
+                typeof(EditableDiceRollUserControl),
+                new FrameworkPropertyMetadata(PropertyPath_PropertyChanged));
+
+        protected static void PropertyPath_PropertyChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var ctl = d as EditableDiceRollUserControl;
+
+            var binding = new Binding(ctl.PropertyPath)
+            {
+                ValidationRules = { new DiceRollRule() },
+
+                //  Optional. With this, the bound property will be updated and validation 
+                //  will be applied on every keystroke. 
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+                Converter = new StringToDiceRollConverter(),
+            };
+
+            ctl.textBox.SetBinding(TextBox.TextProperty, binding);
+
+        }
+
+        #region IIsValidable
+
+        /// <summary>
+        ///     Tells wether the validation Rule is in error or not 
+        ///     /!\ The input event triggers before the check, so we can't just check the Error by hand after each input
+        /// </summary>
+        /// <returns></returns>
+        public bool IsValid()
+        {
+            return !Validation.GetHasError(textBox);
+        }
+
+        #endregion
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                System.Windows.Input.KeyEventArgs args = new System.Windows.Input.KeyEventArgs(Keyboard.PrimaryDevice, Keyboard.PrimaryDevice.ActiveSource, 0, Key.Tab);
+                args.RoutedEvent = Keyboard.KeyDownEvent;
+                InputManager.Current.ProcessInput(args);
+            }
+        }
+    }
+}
