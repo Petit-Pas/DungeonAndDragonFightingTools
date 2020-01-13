63a9ee21de221044aad6d5ea8745245d5a9e0461
Benoit <benoit.hoffman@epitech.eu>
2020-01-13 21:00:32 +0100

    DEV  - added Context menu for the PrepareFightCharacterList.xaml

--- DDFight/Controlers/PrepareFightCharacterList.xaml.cs
+++ DDFight/Controlers/PrepareFightCharacterList.xaml.cs
@@ -37,5 +37,51 @@ namespace DDFight.Controlers
         {
             CharactersList.ItemsSource = data_context.FightingCharacters;
         }
+
+        #region Add
+
+        /// <summary>
+        ///     add a new character
+        /// </summary>
+        private void add_fighter(PlayableEntity fighter)
+        {
+            data_context.FightingCharacters.Add(fighter);
+
+        }
+
+        private void DuplicateFighter_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            PlayableEntity fighter = (PlayableEntity)CharactersList.SelectedItem;
+            if (fighter is MonsterDataContext)
+            {
+                IEnumerable<PlayableEntity> list = data_context.FightingCharacters.Where(x => x.Name == ((MonsterDataContext)CharactersList.SelectedItem).Name);
+                PlayableEntity new_fighter = (PlayableEntity)(((MonsterDataContext)CharactersList.SelectedItem).Clone());
+
+                new_fighter.DisplayName = new_fighter.Name + " - " + list.Count().ToString();
+                add_fighter(new_fighter);
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        private void delete_fighter(PlayableEntity entity)
+        {
+            data_context.FightingCharacters.Remove(entity);
+        }
+
+        private void DeleteFighter_MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            delete_fighter((PlayableEntity)CharactersList.SelectedItem);
+        }
+
+        #endregion
+
+        private void CharactersList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+                this.DeleteFighter_MenuItem_Click(sender, null);
+        }
     }
 }
