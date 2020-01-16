597e6f20e8630c82e7c950be946a38f4a1dfb7e7
Benoit <benoit.hoffman@epitech.eu>
2020-01-16 15:54:12 +0100

    WIP - Started ListStringStringConverter to be used for the log console, has not been tested yet

---
+++ DDFight/Converters/ListStringStringConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DDFight.Converters
+{
+    public class ListStringStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // from List<string> to string
+            List<string> list = (List<string>)value;
+            string result = "";
+
+            foreach (string str in list)
+            {
+                result += str + "\r\n";
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // from string to List<string>
+
+            string full = (string)value;
+            List<string> result = new List<string>();
+
+            int start = 0;
+
+            while (true)
+            {
+                string tmp = "";
+                int next = full.IndexOf("\r\n", start);
+                if (next == -1)
+                {
+                    tmp = full.Substring(start);
+                    result.Add(tmp);
+                    break;
+                }
+                tmp = full.Substring(start, next - start);
+                start = next + 2;
+                result.Add(tmp);
+            }
+            return result;
+        }
+    }
+}
