f0f6eb96660ddb047256435ec7972283abb5f6e1
Benoit <benoit.hoffman@epitech.eu>
2020-02-09 13:49:25 +0100

    DEV - Added more generic UserControls for DamageListUserControl

--- DDFight/Controlers/Game/Attacks/EditableHitAttackTemplateUserControl.xaml.cs
+++ DDFight/Controlers/Game/Attacks/EditableHitAttackTemplateUserControl.xaml.cs
@@ -33,45 +33,6 @@ namespace DDFight.Controlers.Game.Attacks
         public EditableHitAttackTemplateUserControl()
         {
             InitializeComponent();
-
-            Loaded += EditableHitAttackTemplate_Loaded;
-        }
-
-
-        private void EditableHitAttackTemplate_Loaded(object sender, RoutedEventArgs e)
-        {
-            refresh_damage_list();
-        }
-
-        private void refresh_damage_list()
-        {
-            List<DamageTemplate> list = new List<DamageTemplate>();
-            foreach (DamageTemplate tmp in _dataContext.DamageList)
-            {
-                list.Add(tmp);
-            }
-            DamageListView.ItemsSource = list;
-        }
-
-        private void AddDamage_Button_Click(object sender, RoutedEventArgs e)
-        {
-            _dataContext.DamageList.Add(new DamageTemplate("1d4", DamageTypeEnum.Force));
-            refresh_damage_list();
-        }
-
-        private void RemoveDamage_Button_Click(object sender, RoutedEventArgs e)
-        {
-            int index = 0;
-            foreach (DamageTemplate tmp in DamageListView.ItemsSource)
-            {
-                if (tmp == ((Button)sender).DataContext)
-                {
-                    _dataContext.DamageList.RemoveAt(index);
-                    break;
-                }
-                index += 1;
-            }
-            refresh_damage_list();
         }
 
         public bool IsValid()
