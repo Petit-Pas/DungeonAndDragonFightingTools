524b01ed81cab7625cfc75542b1cf89a91755a9b
Benoit <benoit.hoffman@epitech.eu>
2020-10-09 23:20:37 +0200

    WIP - CustomSliderButton now slides

---
+++ WpfSandbox/CustomSliderButton.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSandbox
+{
+    /// <summary>
+    /// Logique d'interaction pour CustomSliderButton.xaml
+    /// </summary>
+    public partial class CustomSliderButton : UserControl, INotifyPropertyChanged
+    {
+        public CustomSliderButton()
+        {
+            InitializeComponent();
+        }
+
+        #region INotifyPropertyChanged
+
+        /// <summary>
+        ///     PropertyChanged EventHandler
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
