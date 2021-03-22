380358c35abb72ec6d3783564cb66080b02c79f7
Benoît HOFFMAN <benoit.hoffman@epitech.eu>
2021-03-22 16:11:09 +0100

    UI/UX - Updated shadow of CircularSelectorRadioButtonStyle

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -9,41 +9,25 @@ namespace SandBox
 {
     class Program
     {
+        public class Test
+        {
+            public int test = 0;
+
+            public Test()
+            {
+                Console.WriteLine("base ctor");
+                test = 1;
+            }
+
+            public Test(int oui) : this()
+            {
+                Console.WriteLine("allo?");
+            }
+        }
+
         static void Main(string[] args)
         {
-
-            string text = "1d4+3d6+8";
-            //string pat = @"^((?:\-?[0-9]+)|(?:|\-?[0-9]+d[0-9]+))(?:((?:\+|\-)[0-9]+)|((?:\+|\-)[0-9]+d[0-9]+))*$";
-            string pat = @"^((?:\-?[0-9]+)|(?:|\-?[0-9]+d[0-9]+))((?:(?:\+|\-)[0-9]+)|(?:(?:\+|\-)[0-9]+d[0-9]+))*$";
-
-            // Instantiate the regular expression object.
-            Regex r = new Regex(pat, RegexOptions.IgnoreCase);
-
-            // Match the regular expression pattern against a text string.
-            Match match = r.Match(text);
-            if (match.Success)
-                try
-                {
-                    for (int i = 1; i != match.Groups.Count; i += 1)
-                    {
-                        foreach (Capture capture in match.Groups[i].Captures)
-                        {
-                            string captured = capture.ToString();
-                            if (captured.Contains("d"))
-                            {
-                                Console.WriteLine("adding dice {0}", captured);
-                            }
-                            else
-                            {
-                                Console.WriteLine("adding mod {0}", captured);
-                            }
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("error");
-                }
+            Test test = new Test(0);
 
             Console.ReadKey();
         }
