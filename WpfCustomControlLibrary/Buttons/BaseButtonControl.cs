6ba33ad58e7bb355379956cf56e019351726ea90
Benoit <benoit.hoffman@epitech.eu>
2021-04-13 23:40:02 +0200

    DEV - Added a BaseButton from which ExitButton inherits

---
+++ WpfCustomControlLibrary/Buttons/BaseButtonControl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfCustomControlLibrary.Buttons
+{
+    public class BaseButtonControl : Button
+    {
+        private static readonly ResourceDictionary styleDict = new ResourceDictionary()
+        {
+            Source = new Uri("/WpfCustomControlLibrary;component/Buttons/BaseButtonStyle.xaml", UriKind.RelativeOrAbsolute)
+        };
+        private static readonly Style style = styleDict["BaseButtonStyle"] as Style;
+
+        public BaseButtonControl() : base()
+        {
+            if (style != null)
+                this.Style = style;
+        }
+
+        public Brush BaseColor
+        {
+            get { return (Brush)this.GetValue(BaseColorProperty); }
+            set { this.SetValue(BaseColorProperty, value); }
+        }
+        private static readonly DependencyProperty BaseColorProperty = DependencyProperty.Register(
+            nameof(BaseColor),
+            typeof(Brush),
+            typeof(BaseButtonControl),
+            new PropertyMetadata(System.Windows.Application.Current.Resources["Gray"]));
+
+        public Brush HoverColor
+        {
+            get { return (Brush)this.GetValue(HoverColorProperty); }
+            set { this.SetValue(HoverColorProperty, value); }
+        }
+        private static readonly DependencyProperty HoverColorProperty = DependencyProperty.Register(
+            nameof(HoverColor),
+            typeof(Brush),
+            typeof(BaseButtonControl),
+            new PropertyMetadata(System.Windows.Application.Current.Resources["Indigo"]));
+
+        public Brush ClickColor
+        {
+            get { return (Brush)this.GetValue(ClickColorProperty); }
+            set { this.SetValue(ClickColorProperty, value); }
+        }
+        private static readonly DependencyProperty ClickColorProperty = DependencyProperty.Register(
+            nameof(ClickColor),
+            typeof(Brush),
+            typeof(BaseButtonControl),
+            new PropertyMetadata(System.Windows.Application.Current.Resources["LightIndigo"]));
+
+    }
+}
---
+++ WpfCustomControlLibrary/Buttons/ExitButtonControl.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using WpfToolsLibrary.Commands.WindowCommands;
+using WpfToolsLibrary.Extensions;
+
+namespace WpfCustomControlLibrary.Buttons
+{
+    public class ExitButtonControl : BaseButtonControl
+    {
+        public ExitButtonControl() : base()
+        {
+            BaseColor = Brushes.DarkRed;
+            HoverColor = Brushes.Red;
+            ClickColor = Brushes.PaleVioletRed;
+            Content = new TextBlock() {
+                Text = "X",
+                Foreground = (Brush)System.Windows.Application.Current.Resources["Light"],
+                FontSize = 15,
+                FontWeight = FontWeights.DemiBold,
+            };
+            Click += ExitButtonControl_Click;
+            CloseCommand_Click = new CloseCommand();
+        }
+
+        private void ExitButtonControl_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CloseCommand_Click.Execute(Application.Current.GetCurrentWindow());
+        }
+
+        public CloseCommand CloseCommand_Click { get; set; }
+    }
+}
