a360662c250ff4ec34252e56dec08f2aefb7aec0
Benoit <benoit.hoffman@epitech.eu>
2021-04-22 21:52:21 +0200

    UI/UX - Added animation in CheckBoxes

--- WpfCustomControlLibrary/CheckBoxes/CheckBoxControl.cs
+++ WpfCustomControlLibrary/CheckBoxes/CheckBoxControl.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 
 namespace WpfCustomControlLibrary.CheckBoxes
 {
@@ -29,6 +31,47 @@ namespace WpfCustomControlLibrary.CheckBoxes
                 Style = style;
             if (checkMark != null)
                 CheckMark = checkMark;
+            Checked += CheckBoxControl_Checked;
+            Unchecked += CheckBoxControl_Unchecked;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            init();
+        }
+
+        private bool initializing = false;
+
+        private void init()
+        {
+            initializing = true;
+            Path path = this.Template.FindName("CheckMark", this) as Path;
+            if (this.IsChecked.Value == true)
+            {
+                CheckBoxControl_Checked(this, null);
+            }
+            else
+                CheckBoxControl_Unchecked(this, null);
+            if (path != null)
+                path.Visibility = Visibility.Visible;
+            initializing = false;
+        }
+
+        private void CheckBoxControl_Checked(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation(15, 0, TimeSpan.FromSeconds(initializing ? 0.0 : 0.15));
+            Path path = this.Template.FindName("CheckMark", this) as Path;
+            if (path != null)
+                path.BeginAnimation(Path.StrokeDashOffsetProperty, animation);
+        }
+
+        private void CheckBoxControl_Unchecked(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation(0, 15, TimeSpan.FromSeconds(initializing ? 0.0 : 0.15));
+            Path path = this.Template.FindName("CheckMark", this) as Path;
+            if (path != null)
+                path.BeginAnimation(Path.StrokeDashOffsetProperty, animation);
         }
 
         public Brush BackgroundColor
