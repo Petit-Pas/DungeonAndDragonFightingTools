d7d41175db2abf10ea996874d63daa4324f5b0e8
Benoît HOFFMAN <benoit.hoffman@epitech.eu>
2021-05-29 11:28:04 +0200

    DEV - Enhanced UX on ComboBoxControl with keybaord utilisation

--- WpfCustomControlLibrary/ComboBoxes/ComboBoxControl.cs
+++ WpfCustomControlLibrary/ComboBoxes/ComboBoxControl.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace WpfCustomControlLibrary.ComboBoxes
 {
     public class ComboBoxControl : ComboBox
     {
+    
         private static readonly ResourceDictionary styleDict = new ResourceDictionary()
         {
             Source = new Uri("/WpfCustomControlLibrary;component/ComboBoxes/ComboBoxStyles.xaml", UriKind.RelativeOrAbsolute)
@@ -24,9 +28,22 @@ namespace WpfCustomControlLibrary.ComboBoxes
             if (this.ItemContainerStyle == null && itemStyle != null)
                 this.ItemContainerStyle = itemStyle;
             this.FocusVisualStyle = null;
+            this.KeyDown += ComboBoxControl_KeyDown;
+            this.DropDownClosed += ComboBoxControl_DropDownClosed;
         }
-        
-        
 
+        private void ComboBoxControl_DropDownClosed(object sender, EventArgs e)
+        {
+            ToggleButton btn =  this.Template.FindName("ToggleButton", this) as ToggleButton;
+            Border border = btn.Template.FindName("Border", btn) as Border;
+            ;
+            border.Focus();
+        }
+
+        private void ComboBoxControl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space || e.Key == Key.Enter)
+                this.IsDropDownOpen = true;
+        }
     }
 }
---
+++ WpfDnDCustomControlLibrary/Attacks/Damage/DamageAffinity/DamageAffinitySelectableControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DnDToolsLibrary.Attacks.Damage.Type;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDnDCustomControlLibrary.Attacks.Damage.DamageAffinity
+{
+    /// <summary>
+    /// Interaction logic for DamageAffinitySelectableControl.xaml
+    /// </summary>
+    public partial class DamageAffinitySelectableControl : UserControl
+    {
+        public DamageAffinitySelectableControl()
+        {
+            InitializeComponent();
+        }
+
+        public DamageAffinityEnum DamageAffinity
+        {
+            get { return (DamageAffinityEnum)this.GetValue(DamageAffinityProperty); }
+            set { this.SetValue(DamageAffinityProperty, value); }
+        }
+
+        public static readonly DependencyProperty DamageAffinityProperty = DependencyProperty.Register(
+            nameof(DamageAffinity),
+            typeof(DamageAffinityEnum),
+            typeof(DamageAffinitySelectableControl),
+            new PropertyMetadata(DamageAffinityEnum.Neutral)
+        );
+    }
+}
