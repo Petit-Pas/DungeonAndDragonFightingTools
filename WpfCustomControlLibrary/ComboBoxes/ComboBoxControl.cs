a61edbb46f09edb3cb0cff8ca894f796719ea4cb
Benoît HOFFMAN <benoit.hoffman@epitech.eu>
2021-06-05 08:42:57 +0200

    WIP - Moving the animations in ComboBox from inner Control to main one

--- WpfCustomControlLibrary/ComboBoxes/ComboBoxControl.cs
+++ WpfCustomControlLibrary/ComboBoxes/ComboBoxControl.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using WpfToolsLibrary.Extensions;
 
 namespace WpfCustomControlLibrary.ComboBoxes
 {
@@ -30,14 +31,47 @@ namespace WpfCustomControlLibrary.ComboBoxes
             this.FocusVisualStyle = null;
             this.KeyDown += ComboBoxControl_KeyDown;
             this.DropDownClosed += ComboBoxControl_DropDownClosed;
+            this.MouseEnter += ComboBoxControl_MouseEnter;
+            this.MouseLeave += ComboBoxControl_MouseLeave;
+            this.DropDownClosed += ComboBoxControl_DropDownClosed1;
+            this.GotFocus += ComboBoxControl_GotFocus;
+            this.LostFocus += ComboBoxControl_LostFocus;
+        }
+
+        private void ComboBoxControl_LostFocus(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("LostFocus");
+        }
+
+        private void ComboBoxControl_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("GotFocus");
+        }
+
+        private void ComboBoxControl_DropDownClosed1(object sender, EventArgs e)
+        {
+            Console.WriteLine("Closed");
+        }
+
+        private void ComboBoxControl_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("MouseLeave");
+
+        }
+
+        private void ComboBoxControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("MouseEnter");
         }
 
         private void ComboBoxControl_DropDownClosed(object sender, EventArgs e)
         {
-            ToggleButton btn =  this.Template.FindName("ToggleButton", this) as ToggleButton;
+            /*ToggleButton btn =  this.Template.FindName("ToggleButton", this) as ToggleButton;
             Border border = btn.Template.FindName("Border", btn) as Border;
             ;
-            border.Focus();
+            border.Focus();*/
+            Application.Current.GetCurrentWindow().Focus();
+            this.Focus();
         }
 
         private void ComboBoxControl_KeyDown(object sender, KeyEventArgs e)
