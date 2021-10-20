80f3ac78e79600239adaffbb2471f76028c1d337
Benoît HOFFMAN <benoit.hoffman@epitech.eu>
2021-10-20 16:42:59 +0200

    WIP - wrote the body of the LoseConcentrationCommandHandler but it needs to be in a separate RemoveStatusCommand

--- DnDToolsLibrary/Entities/EntitiesCommands/ConcentrationCommands/LoseConcentration/LoseConcentrationCommandHandler.cs
+++ DnDToolsLibrary/Entities/EntitiesCommands/ConcentrationCommands/LoseConcentration/LoseConcentrationCommandHandler.cs
@@ -1,5 +1,7 @@
-﻿using BaseToolsLibrary.Mediator;
+﻿using BaseToolsLibrary.DependencyInjection;
+using BaseToolsLibrary.Mediator;
 using DnDToolsLibrary.Fight;
+using DnDToolsLibrary.Status;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,7 @@ namespace DnDToolsLibrary.Entities.EntitiesCommands.ConcentrationCommands.LoseCo
     public class LoseConcentrationCommandHandler : SuperCommandHandlerBase<LoseConcentrationCommand, NoResponse>
     {
         private IStatusProvider _statusProvider { get => __lazyStatusProvider.Value; }
-        private Lazy<IStatusProvider> __lazyStatusProvider;
+        private Lazy<IStatusProvider> __lazyStatusProvider = new Lazy<IStatusProvider>(() => DIContainer.GetImplementation<IStatusProvider>());
 
         public override NoResponse Execute(IMediatorCommand command)
         {
@@ -23,9 +25,18 @@ namespace DnDToolsLibrary.Entities.EntitiesCommands.ConcentrationCommands.LoseCo
                 return MediatorCommandResponses.NoResponse;
             }
 
-            _statusProvider.GetOnHitStatusesAppliedBy(entity.DisplayName);
-            // remove the statuses that were applied by entity AND required concentration
+            // removes the statuses that were applied by entity AND required concentration
+            var statuses = _statusProvider.GetOnHitStatusesAppliedBy(entity.DisplayName).Where(x => x.EndsOnCasterLossOfConcentration);
+            foreach (OnHitStatus status in statuses)
+            {
+                //TODO this should be in a command by itself
 
+                // remove from status provider
+                _statusProvider.Remove(status);
+                // remove from affected
+                StatusReference reference = status.Affected.AffectingStatusList.First(x => x.ActualStatusReferenceId == status.Id);
+                status.Affected.AffectingStatusList.Remove(reference);
+            }
             return MediatorCommandResponses.NoResponse;
         }
     }
